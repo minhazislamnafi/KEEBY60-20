10aa7439febe4ca9158efb4a20b239e7c9b9a5d9
Minhaz Islam Nafi <minhazislam2211@gmail.com>
2026-06-05 23:37:15 -0500

    Create config.h

---
+++ Firmware/KEEBY60/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP0
+#define I2C1_SCL_PIN GP1
+
+// OLED
+#define OLED_DISPLAY_128X32
+#define OLED_DRIVER_ENABLE
+
+#define ENCODER_RESOLUTION 4
+#define ENCODER_A_PINS {GP20}
+#define ENCODER_B_PINS {GP21}
+
+// Tapping time term || mod-tap keys (ms)
+#define TAPPING_TERM 200
